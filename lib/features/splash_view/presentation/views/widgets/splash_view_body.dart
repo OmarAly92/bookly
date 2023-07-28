f919a7c97a2f4f726fa3c150328e35c93b6b316a
Omar <omarplaystation8888@gmail.com>
2023-07-28 04:26:47 +0300

    adding sliding animation to splash view

--- lib/features/splash_view/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash_view/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,30 @@
 import 'package:bookly/core/assets_data.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
-  const SplashViewBody({super.key});
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({Key? key}) : super(key: key);
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    slidingText();
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -10,9 +32,29 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Image.asset(AssetsData.logoImage),
-        const Text('Read free books',textAlign: TextAlign.center),
+        Image.asset(AssetsData.logoImage, filterQuality: FilterQuality.high),
+        AnimatedBuilder(
+            animation: slidingAnimation,
+            builder: (context, child) {
+              return SlideTransition(
+                position: slidingAnimation,
+                child: const Text(
+                  'Read Free Books',
+                  textAlign: TextAlign.center,
+                ),
+              );
+            })
       ],
     );
   }
+  void slidingText() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 1.5), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+
 }
