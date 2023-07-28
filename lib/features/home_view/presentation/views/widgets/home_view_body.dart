f8049ca2879432d3c37864078476489c8eabab5e
Omar <omarplaystation8888@gmail.com>
2023-07-28 20:38:34 +0300

    best seller listView item part 1

--- lib/features/home_view/presentation/views/widgets/home_view_body.dart
+++ lib/features/home_view/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly/core/assets_data.dart';
 import 'package:bookly/core/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'custom_app_bar.dart';
 import 'featured_books_list_view.dart';
@@ -16,15 +17,15 @@ class HomeViewBody extends StatelessWidget {
         children: [
           CustomAppBar(),
           FeaturedBooksListView(),
-          CustomWidget1(),
+          BestSellerListViewItem(),
         ],
       ),
     );
   }
 }
 
-class CustomWidget1 extends StatelessWidget {
-  const CustomWidget1({super.key});
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -33,52 +34,58 @@ class CustomWidget1 extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-           Text(
+          Text(
             'Best Seller',
             style: Styles.titleMedium,
           ),
-          const SizedBox(height: 20),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.asset(
-                  AssetsData.testImage,
-                  height: 150,
-                  width: 100,
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 15),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: 200,
-                      child: const Text(
-                        'Harry Potter and the Goblet of Fire',
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(fontSize: 20),
+          SizedBox(height: 20.h),
+          SizedBox(
+            height: 125,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AspectRatio(
+                  aspectRatio: 2 / 3,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(15),
+                      image: const DecorationImage(
+                        image: NetworkImage(AssetsData.testNetworkImage),
+                        fit: BoxFit.fill,
                       ),
                     ),
-                    const Text('J.K Rowling'),
-                    const Row(
-                      children: [
-                        Text('19.99 E'),
-                        Row(
-                          children: [
-                            Icon(Icons.star, color: Colors.yellow),
-                            Text('4.8 (2390)')
-                          ],
-                        )
-                      ],
-                    )
-                  ],
+                  ),
                 ),
-              )
-            ],
+
+                // Column(
+                //   mainAxisAlignment: MainAxisAlignment.start,
+                //   children: [
+                //     SizedBox(
+                //       width: 200,
+                //       child: const Text(
+                //         'Harry Potter and the Goblet of Fire',
+                //         maxLines: 2,
+                //         overflow: TextOverflow.ellipsis,
+                //         style: TextStyle(fontSize: 20),
+                //       ),
+                //     ),
+                //     const Text('J.K Rowling'),
+                //     const Row(
+                //       children: [
+                //         Text('19.99 E'),
+                //         Row(
+                //           children: [
+                //             Icon(Icons.star, color: Colors.yellow),
+                //             Text('4.8 (2390)')
+                //           ],
+                //         )
+                //       ],
+                //     )
+                //   ],
+                // )
+              ],
+            ),
           )
         ],
       ),
