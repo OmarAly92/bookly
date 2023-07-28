7f75effbdd0b82b7613d98bfa1ac37035585b546
Omar <omarplaystation8888@gmail.com>
2023-07-28 20:20:26 +0300

    create styles file and title in the home view

--- lib/core/assets_data.dart
+++ lib/core/assets_data.dart
@@ -1,4 +1,5 @@
 class AssetsData {
   static const String logoImage = 'assets/images/Logo.png';
   static const String testImage = 'assets/images/test_image.png';
+  static const String testNetworkImage = 'http://books.google.com/books/content?id=xMYZfStolfcC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api';
 }
---
+++ lib/core/styles.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+abstract class Styles{
+  static TextStyle titleMedium = TextStyle(fontSize: 19.sp,fontWeight: FontWeight.w600);
+}
