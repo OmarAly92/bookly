6d83eb5d1de2e4b9abd8b4e044c21dcfa41c6ce7
Omar <omarplaystation8888@gmail.com>
2023-07-31 01:10:27 +0300

    refactor BookDetailsViewBody

---
+++ lib/features/home_view/presentation/views/widgets/book_details_section.dart
@@ -0,0 +1,51 @@
+import 'package:bookly/core/constants.dart';
+import 'package:bookly/core/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'book_rating.dart';
+import 'box_action.dart';
+import 'custom_book_data_item.dart';
+
+class BookDetailsSection extends StatelessWidget {
+  const BookDetailsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 80.w),
+          child: const CustomBookDataItem(),
+        ),
+        const SizedBox(
+          height: 23,
+        ),
+        Text(
+          'The Jungle Book',
+          style: Styles.textStyle30,
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        Text(
+          'Rudyard Kipling',
+          style: Styles.textStyle18.copyWith(
+            color: kGreySubtitle,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+        const SizedBox(
+          height: 18,
+        ),
+        const BookRating(
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+        const SizedBox(
+          height: 37,
+        ),
+        const BoxAction(),
+      ],
+    );
+  }
+}
