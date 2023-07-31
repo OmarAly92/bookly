4d4d5a8071821b0c66aa9894a5b8404b401f3b21
Omar <omarplaystation8888@gmail.com>
2023-07-31 08:00:32 +0300

    create server failure from dio error

--- lib/features/home_view/data/repository/home_repository.dart
+++ lib/features/home_view/data/repository/home_repository.dart
@@ -2,6 +2,7 @@ import 'package:bookly/core/errors/failures.dart';
 import 'package:bookly/features/home_view/data/data_source/home_view_api_request/home_view_api_request.dart';
 import 'package:bookly/features/home_view/data/models/book_model.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 abstract class BaseHomeRepository {
   Future<Either<Failure, List<BookModel>>> fetchNewBooks();
@@ -18,12 +19,13 @@ class HomeRepository implements BaseHomeRepository {
   Future<Either<Failure, List<BookModel>>> fetchNewBooks() async {
     try {
       var data = await homeViewApiRequest.getBooksData(
-          endPoint:
-              'volumes?Filtering=free-ebooks&Sorting=newest &q=subject:Programming');
-
+          endPoint: 'volumes?Filtering=free-ebooks&Sorting=newest &q=subject:Programming');
       return Right(data);
     } catch (e) {
-      return Left(ServerFailure());
+      if (e is DioException) {
+        return Left(ServerFailure.fromDioError(e));
+      }
+      return Left(ServerFailure(e.toString()));
     }
   }
 
